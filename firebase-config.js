07827aa4ff9f22a84413735d21a2b84b72ba56bb
Vishva-0628 <delzizerrin@gmail.com>
2026-03-14 20:00:43 +0530

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey:            "AIzaSyAEz9rBlBEaLJSzCgLCKMtS5radz4GUE14",
+  authDomain:        "sivasakthi-marketing.firebaseapp.com",
+  projectId:         "sivasakthi-marketing",
+  storageBucket:     "sivasakthi-marketing.firebasestorage.app",
+  messagingSenderId: "144894314589",
+  appId:             "1:144894314589:web:b6f68f1b5a23f87ede25b4"
+};
+
+// IMPORTANT: put your exact Gmail address here
+const ADMIN_EMAILS = ["vetrivishva217@gmail.com"];
